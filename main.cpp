adce5d9bd591c45e39cd1d96f11726082e6fe0ba
立石義弘 <115062998+tateishiyoshihiro@users.noreply.github.com>
2024-02-20 15:15:49 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -7,9 +7,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
-
 	GameManager* gameManager = new GameManager();
-
 	gameManager->Run();
 
 	// ライブラリの終了
